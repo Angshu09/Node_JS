4cb3b854871901384e393f57b096015647e623ea
Angshu09 <angshudas191@gmail.com>
2025-08-01 21:56:48 +0530

    Done with How Node works

---
+++ 04_HowNodeWorks/index.js
@@ -0,0 +1,26 @@
+import fs from 'node:fs'
+
+// console.log('1');
+// const res = fs.readFileSync('./test.txt', 'utf-8')
+// console.log(res);
+// console.log('2');
+
+//O/P
+// 1
+// Name - Angshu Das
+// College - Maulana Abul Kalam Azad University Of Technology
+// Mail - angshudas191@gmail.com
+// 2
+
+// console.log('1');
+// fs.readFile('./test.txt', 'utf-8', (err, res) => {
+//     err ? console.log(err) : console.log(res);
+// })
+// console.log('2');
+
+// O/P
+// 1
+// 2
+// Name - Angshu Das
+// College - Maulana Abul Kalam Azad University Of Technology
+// Mail - angshudas191@gmail.com
