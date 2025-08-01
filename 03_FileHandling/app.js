db283fd292b2327705ea96b5fb74398911df5c2b
Angshu09 <angshudas191@gmail.com>
2025-08-01 21:44:56 +0530

    Done with file handling

---
+++ 03_FileHandling/app.js
@@ -0,0 +1,26 @@
+import fs from "node:fs";
+
+// fs.writeFileSync('./helloSync.txt', 'Hey Hello there sync!');
+// fs.writeFile('./helloAsync.txt', 'Hey Hello there async!', (err, res) => console.log(err));
+
+// const res = fs.readFileSync("./helloSync.txt", "utf-8");
+// console.log(res);
+// fs.readFile("./helloAsync.txt", "utf-8", (err, res) => {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log(res);
+//   }
+// });
+
+// fs.appendFileSync('./helloSync.txt', `\n ${Date.now()} Hey there`)
+
+// fs.cpSync('./helloSync.txt', './copy.txt')
+
+// const stats = fs.statSync('./helloSync.txt')
+// console.log(stats)
+
+// fs.unlinkSync('./copy.txt')
+
+// fs.mkdirSync('./myDocs')
+// fs.mkdirSync('./myDocss/a/b', {recursive: true})
