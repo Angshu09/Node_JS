bd21020aaca0f5350abe6e72366cd245981820b5
Angshu09 <angshudas191@gmail.com>
2025-08-01 19:40:49 +0530

    Done with 02_Modules

---
+++ 02_Modules/app.js
@@ -0,0 +1,4 @@
+const { add, sub } = require("./math");
+
+console.log(add(2, 2));
+console.log(sub(8, 4));
---
+++ 02_Modules/math.js
@@ -0,0 +1,15 @@
+const add = (a, b) => {
+  return a + b;
+};
+
+const sub = (a, b) => {
+  return a - b;
+};
+
+// exports.add = add;
+// exports.sub = sub;
+
+module.exports = {
+  add,
+  sub,
+};
